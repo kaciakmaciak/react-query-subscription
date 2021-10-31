4d3f7d004df3b2ee92b8d92730808678e5480f8c
Katarina Anton <kaciak.maciak@gmail.comcd>
2021-10-31 13:05:06 +1300

    feat(types): export `UseSubscriptionOptions` and `EventSourceOptions` types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { useSubscription } from './use-subscription';
+export type { UseSubscriptionOptions } from './use-subscription';
 
 export { eventSource$, fromEventSource } from './helpers/event-source';
+export type { EventSourceOptions } from './helpers/event-source';
