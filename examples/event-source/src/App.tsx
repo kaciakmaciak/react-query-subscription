73202808265eda6ab000a5c3c4b9e73baaf3bcae
Katarina Anton <kaciak.maciak@gmail.comcd>
2021-10-31 16:21:48 +1300

    docs(examples): add basic event source example

---
+++ examples/event-source/src/App.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { useSubscription, eventSource$ } from 'react-query-subscription';
+
+interface Props {
+  subscriptionKey: string;
+  initiallyEnabled?: boolean;
+}
+
+function Test({ subscriptionKey, initiallyEnabled }: Props) {
+  const [enabled, setEnabled] = useState(Boolean(initiallyEnabled));
+  const { data, isError, error, isLoading, isIdle, refetch } = useSubscription(
+    subscriptionKey,
+    () => eventSource$('http://localhost:8080/sse'),
+    { enabled }
+  );
+
+  if (isIdle || !enabled) {
+    return <button onClick={() => setEnabled(true)}>Enable</button>;
+  }
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+  if (isError) {
+    return (
+      <div role="alert">
+        {error?.message || 'Unknown error'}{' '}
+        <button onClick={() => refetch()}>Retry</button>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <button onClick={() => refetch()}>Refetch</button>
+      Test: {JSON.stringify(data)}
+    </>
+  );
+}
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>EventSource Example</h1>
+      <p>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/API/EventSource">
+          EventSource on MDN
+        </a>
+      </p>
+      <p>
+        When component is mounted and subscription is enabled, we subscribe to
+        EventSource at <code>http://localhost:8080/sse</code> and stream
+        messages as JSON data.
+      </p>
+      <p>Once no components are subscribed, EventSource is closed.</p>
+      <p>
+        Notice, that first two components have the same subscription keys.
+        Therefore they share the EventSource subscription (and only one
+        EventSource will be created for both of them).
+      </p>
+      <p>
+        The third component has a unique subscription key, hence is independent
+        from other two.
+      </p>
+      <div>
+        <button onClick={() => setShow1((v) => !v)}>
+          {show1 ? 'Unmount' : 'Mount'} (Initially disabled)
+        </button>{' '}
+        {show1 && <Test subscriptionKey="event-source-test-key" />}
+      </div>
+      <div>
+        <button onClick={() => setShow2((v) => !v)}>
+          {show2 ? 'Unmount' : 'Mount'} (Initially enabled)
+        </button>{' '}
+        {show2 && (
+          <Test subscriptionKey="event-source-test-key" initiallyEnabled />
+        )}
+      </div>
+      <hr />
+      <div>
+        <button onClick={() => setShow3((v) => !v)}>
+          {show3 ? 'Unmount' : 'Mount'} (Independent)
+        </button>{' '}
+        {show3 && <Test subscriptionKey="unique" initiallyEnabled />}
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
