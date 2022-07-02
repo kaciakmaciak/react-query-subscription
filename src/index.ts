dd88106a039a58c26a2352b5656bb1a0ce236406
Katarina Anton <kaciak.maciak@gmail.comcd>
2022-07-02 21:04:39 +1200

    feat(useInfiniteSubscription): add `useInfiniteSubscription` hook
    resolve #55

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { useSubscription } from './use-subscription';
 export type { UseSubscriptionOptions } from './use-subscription';
+export { useInfiniteSubscription } from './use-infinite-subscription';
+export type { UseInfiniteSubscriptionOptions } from './use-infinite-subscription';
 
 export { eventSource$, fromEventSource } from './helpers/event-source';
 export type { EventSourceOptions } from './helpers/event-source';
