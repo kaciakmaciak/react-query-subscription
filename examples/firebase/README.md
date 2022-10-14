7c1ef772813a795a55af499d3d72cdf08be75b91
kaciakmaciak <kaciak.maciak@gmail.com>
2022-10-14 23:13:17 +0000

    docs(examples): update firebase example to use `.env`

--- examples/firebase/README.md
+++ examples/firebase/README.md
@@ -1,6 +1,24 @@
 # Example
 
-```
-npm install --ignore-scripts
-npm run dev
-```
+1. Create firestore database as per [docs](https://firebase.google.com/docs/firestore/quickstart#create).
+1. Set environment variables:
+
+   ```sh
+   # Copy the template .env file to local .env file
+   cp .env .env.local
+   # Edit .env.local with firebase config.
+   ```
+
+1. Install dependencies (ideally in project root):
+
+   ```sh
+   npm install
+   ```
+
+   > If you are running the command directly in the `examples/firebase` directory, use ` --ignore-scripts`.
+
+1. Run local dev server:
+
+   ```sh
+   npm run dev
+   ```
--- examples/firebase/src/firebase.ts
+++ examples/firebase/src/firebase.ts
@@ -11,10 +11,15 @@ import { getFirestore } from 'firebase/firestore';
  * @see https://firebase.google.com/docs/firestore/quickstart#create
  */
 const firebaseConfig = {
-  // add config here
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-if (Object.keys(firebaseConfig).length === 0) {
-  throw new Error('Configure firebase in `src/firebase.ts`');
+if (Object.values(firebaseConfig).filter(Boolean).length === 0) {
+  throw new Error('Configure env variables in .env.local`');
 }
 
 export const firebaseApp = initializeApp(firebaseConfig);
