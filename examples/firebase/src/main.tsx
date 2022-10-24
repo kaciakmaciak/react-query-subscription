ef91a8ab5cfc219bf4fec091e171d6ab17f245bd
Katarina Anton <kaciak.maciak@gmail.comcd>
2022-10-24 21:21:34 +1300

    docs(examples): import react-query-subscription from local repo

--- examples/firebase/src/main.tsx
+++ examples/firebase/src/main.tsx
@@ -10,7 +10,7 @@ import App from './App';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
